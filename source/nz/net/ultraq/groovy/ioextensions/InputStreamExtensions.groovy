3086efca3692d0b50560babb00281acbdeba82d4
Emanuel Rabina <emanuelrabina@gmail.com>
2022-02-19 18:51:22 +1300

    Add source from various projects

---
+++ source/nz/net/ultraq/groovy/ioextensions/InputStreamExtensions.groovy
@@ -0,0 +1,56 @@
+/* 
+ * Copyright 2022, Emanuel Rabina (http://www.ultraq.net.nz/)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.net.ultraq.groovy.ioextensions
+
+import groovy.transform.stc.ClosureParams
+import groovy.transform.stc.SimpleType
+
+/**
+ * Extensions to Java's {@link InputStream} classes.
+ * 
+ * @author Emanuel Rabina
+ */
+class InputStreamExtensions {
+
+	/**
+	 * Wrap an input stream with a buffered reader and invoke the usual
+	 * {@code withReader} method over it.
+	 * 
+	 * @param self
+	 * @param closure
+	 * @return
+	 */
+	static <T> T withBufferedReader(InputStream self,
+		@ClosureParams(value = SimpleType, options = 'java.io.BufferedReader') Closure<T> closure) {
+
+		return new BufferedInputStream(self).withReader(closure)
+	}
+
+	/**
+	 * Wrap an input stream with a buffered one and invoke the usual
+	 * {@code withStream} method over it.
+	 * 
+	 * @param self
+	 * @param closure
+	 * @return
+	 */
+	static <T> T withBufferedStream(InputStream self,
+		@ClosureParams(value = SimpleType, options = 'java.io.BufferedInputStream') Closure<T> closure) {
+
+		return new BufferedInputStream(self).withStream(closure)
+	}
+}
