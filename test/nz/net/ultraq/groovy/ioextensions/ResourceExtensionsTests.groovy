8c2d21293ffdc673c32b3c6a5e85c434e91dc171
Emanuel Rabina <emanuelrabina@gmail.com>
2023-12-10 15:38:58 +1300

    Add a getResourceAsFile method

--- test/nz/net/ultraq/groovy/ioextensions/ResourceExtensionsTests.groovy
+++ test/nz/net/ultraq/groovy/ioextensions/ResourceExtensionsTests.groovy
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2022, Emanuel Rabina (http://www.ultraq.net.nz/)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,10 +20,29 @@ import spock.lang.Specification
 
 /**
  * Tests for the {@link ResourceExtensions} methods.
- * 
+ *
  * @author Emanuel Rabina
  */
 class ResourceExtensionsTests extends Specification {
+
+	def "#getResourceAsFile - Is a shortcut to new File(ClassLoader.getResource().toURI())"() {
+		given:
+			def resourcePath = 'nz/net/ultraq/groovy/ioextensions/ResourceExtensionsTestsFile.txt'
+		when:
+			def result = getResourceAsFile(resourcePath)
+		then:
+			assert result.text == new File(this.class.classLoader.getResource(resourcePath).toURI()).text
+	}
+
+	def "#getResourceAsFile - Throws an exception if the path leads to nothing"() {
+		given:
+			def resourcePath = 'path/to/nothing'
+		when:
+			getResourceAsFile(resourcePath)
+		then:
+			def ex = thrown(IllegalArgumentException)
+			assert ex.message == "Resource not found: ${resourcePath}"
+	}
 
 	def "#getResourceAsStream - Is a shortcut to ClassLoader.getResourceAsStream"() {
 		given:
@@ -43,7 +62,7 @@ class ResourceExtensionsTests extends Specification {
 			def ex = thrown(IllegalArgumentException)
 			assert ex.message == "Resource not found: ${resourcePath}"
 	}
-	
+
 	def "#getResourceAsText - Is a shortcut to getResourceAsStream().getText()"() {
 		given:
 			def resourcePath = 'nz/net/ultraq/groovy/ioextensions/ResourceExtensionsTestsFile.txt'
